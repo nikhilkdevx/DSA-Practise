0a6220bcc125c8940ae54b72a36bc5080e05e551
nikhilkdevx <nikhilmasti4@gmail.com>
2026-05-28 17:31:54 +0530

    revision

--- reversearray.cpp
+++ reversearray.cpp
@@ -1,29 +1,49 @@
 #include<iostream>
 using namespace std;
 
-void PrintArr(int arr[],int n){
+// void PrintArr(int arr[],int n){
     
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<",";
+//     for(int i = 0;i<n;i++){
+//         cout<<arr[i]<<",";
+//     }
+// }
+
+
+
+// int main(){
+    
+//     int arr[] ={5,4,3,9,2};
+//     int n = sizeof(arr) / sizeof(int);
+//     int copyArr[n];
+//     for(int i=n-1, j=0; i>=0; i--,j++){
+        
+//         copyArr[j] = arr[i];
+       
+//     }
+//     PrintArr(copyArr,n);
+//     cout<<endl;
+//     PrintArr(arr,n);
+   
+    
+    
+    
+//     return 0;
+// }
+void PrintArr(int *ptr,int n){
+    for(int i=0;i<n;i++){
+        cout<<ptr[i]<<",";
     }
 }
 
-
-
 int main(){
-    
-    int arr[] ={5,4,3,9,2};
+    int arr[] = {10,20,30,40,50,60};
     int n = sizeof(arr) / sizeof(int);
     int copyArr[n];
-    for(int i=n-1, j=0; i>=0; i--,j++){
-        
+    for(int i = n-1 , j = 0 ; i>=0 && j < n ; i-- , j++ ){
         copyArr[j] = arr[i];
-       
     }
-    PrintArr(copyArr,n);
+    PrintArr(copyArr , n); 
     cout<<endl;
-    PrintArr(arr,n);
-   
     
     
     
